c23449a98a8759c4d0873f450cb35c92b3fb07ea
Crowdbotics <team@crowdbotics.com>
2022-06-08 18:30:22 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/melvin_test_35646/MainActivity.java
@@ -0,0 +1,15 @@
+package com.melvin_test_35646;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "melvin_test_35646";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "melvin_test_35646",
+  url: "https://melvin_test_35646.botics.co",
+  api: "https://melvin_test_35646.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
